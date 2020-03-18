4cb51ceeaeeb70558a32fe43ed8fd6d996eadcee
HUU <readabook123@protonmail.com>
2020-03-18 17:28:37 -0500

    Add install scripts, get tmux copy+paste working better, more dev env dependencies

--- install.sh
+++ install.sh
@@ -1,12 +1,31 @@
+#!/usr/bin/env bash
+
 # just in case...
 git submodule update --init --recursive
+
+DISTRO_NAME=$(lsb_release -d | awk '{print $2}')
+if [ "${DISTRO_NAME}" = "Ubuntu" ]; then
+    . "${DEVENV}/ubuntu.sh"
+fi
 
 # color scheme for Gnome terminal
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh)"
 
-# Powerline fonts
-git clone https://github.com/powerline/fonts.git --depth=1
-cd fonts
-./install.sh
-cd ..
-rm -rf fonts
+# my fonts
+sudo mkdir -p "/usr/share/fonts/opentype/devenv/"
+sudo cp -r "${DEVENV}/fonts/*" "/usr/share/fonts/opentype/devenv"
+sudo fc-cache -f -v
+
+TERM_PROFILE_ID=`dconf read /org/gnome/terminal/legacy/profiles:/list | sed -n "s/\[\'\(.*\)\']/\1/p"`
+dconf write "/org/gnome/terminal/legacy/profiles:/:${TERM_PROFILE_ID}/font" "'FuraCode Nerd Font Mono 14'"
+
+# node-based development tools
+sudo npm install -g vscode-html-languageserver-bin remark remark-cli remark-stringify remark-frontmatter wcwidth import-js
+
+# python-based development tools
+pip3 install --user flake8 yapf autoflake isort coverage ipython
+
+# basic Data Science
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/install_miniconda.sh
+bash /tmp/install_miniconda.sh
+
---
+++ ubuntu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+sudo apt install -y zsh build-essential python python3 python3-pip nodejs npm vim-gnome vim-gtk plantuml xclip xsel clang meld ncdu tig
