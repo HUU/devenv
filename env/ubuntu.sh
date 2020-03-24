dc8021990d5350fbb57ccc362f62ab7dd9f62a6f
HUU <readabook123@protonmail.com>
2020-03-23 20:52:18 -0500

    Tap caps lock == Ctrl+B

--- env/ubuntu.sh
+++ env/ubuntu.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -e
-
 echo "Using Ubuntu; applying Ubuntu-specific setup"
-sudo apt install -y zsh build-essential python python3 python3-pip nodejs npm vim-gnome vim-gtk plantuml xclip xsel clang meld ncdu tig
 
+# Basic tools for environment
+sudo apt install -y zsh build-essential pkg-config python python3 python3-pip nodejs npm vim-gnome vim-gtk plantuml xclip xsel clang meld ncdu tig
+
+# Xcape
+sudo apt -y install libx11-dev libxtst-dev libxi-dev
+WORKDIR=$(mktemp -d)
+git clone https://github.com/alols/xcape.git "${WORKDIR}"
+cd "${WORKDIR}"
+make
+sudo make install
+cd -
+rm -rf "${WORKDIR}"
--- env/x.sh
+++ env/x.sh
@@ -5,4 +5,8 @@ if [[ ! -e "${HOME}/.Xmodmap" ]]; then
   ln -s "${DEVENV}/Xmodmap.conf" "${HOME}/.Xmodmap"
 fi
 
+sudo cp "${DEVENV}/80_xmodmap" /etc/X11/Xsession.d/
+
+mkdir -p "${HOME}/.config/autostart"
+ln -s "${DEVENV}/xmodmap.desktop" "${HOME}/.config/autostart/10_xmodmap.desktop"
 
